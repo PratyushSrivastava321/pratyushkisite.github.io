1cd7fd4ce176adf86dcf642dcb555fb66dbecded
PratyushSrivastava321 <prats11211@gmail.com>
2023-09-07 12:09:09 +0530

    saycheese

---
+++ Day1.js
@@ -0,0 +1,13 @@
+//variable constants
+
+console.log("Hello Duniya");
+let age=24;
+console.log("This is LET age:"+age);
+age=25;
+console.log("Changed age:"+age);
+var age2=5;
+console.log(age2)
+const age1=23;
+console.log(age);
+age1=4;
+console.log(age)
